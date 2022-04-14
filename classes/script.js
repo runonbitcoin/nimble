5da75fe79b5aeb03f97f23d67ed2fd8eae670158
Brenton Gunning <brentongunning@gmail.com>
2022-04-14 10:51:28 -0600

    Script templates

--- classes/script.js
+++ classes/script.js
@@ -14,15 +14,22 @@ class Script {
   constructor (buffer = []) {
     if (!isBuffer(buffer)) throw new Error(`Not a buffer: ${buffer}`)
 
-    try {
-      Object.freeze(buffer)
-    } catch (e) {
-      buffer = Uint8Array.from(buffer)
-      Object.freeze(this.buffer)
+    // Store the buffer. If a view, convert to standlone buffer, so that we can make immutable
+    this.buffer = ArrayBuffer.isView(buffer) ? Array.from(buffer) : buffer
+
+    const isTemplate = this.constructor !== Script
+    if (isTemplate) {
+      // If we are using a template, make sure it matches, and that there is no custom constructor
+      if (this.matches(buffer)) throw new Error(`Not a ${this.constructor.name}`)
+      if (this.constructor.toString().includes('constructor')) throw new Error('Template constructors not allowed')
+    } else {
+      // If we are not using a template, see if we can detect one
+      const T = Object.values(Script.templates).find(template => template.matches(buffer))
+      if (T) Object.setPrototypeOf(this, T.prototype)
     }
 
-    this.buffer = buffer
-
+    // Make the script immutable, in part so that its safe to cache chunks
+    Object.freeze(this.buffer)
     Object.freeze(this)
 
     // Proxy the script so it may be used in place of a buffer in functions
@@ -35,10 +42,18 @@ class Script {
     })
   }
 
-  static fromString (s) { return Script.fromHex(s) }
-  static fromHex (s) { return new Script(decodeHex(s)) }
-  static fromBuffer (buffer) { return new Script(buffer) }
-  static fromAddress (address) { return new Script(createP2PKHLockScript(address.toString())) }
+  static fromString (s) {
+    return Script.fromHex(s)
+  }
+
+  static fromHex (s) {
+    return new Script(decodeHex(s))
+  }
+
+  static fromBuffer (buffer) {
+    return new Script(buffer)
+  }
+
   static from (script) {
     if (script instanceof Script) return script
     if (typeof script === 'string') return Script.fromString(script)
@@ -46,15 +61,20 @@ class Script {
     throw new Error(`Invalid script: ${script}`)
   }
 
-  toString () { return this.toHex() }
-  toHex () { return encodeHex(this.buffer) }
-  toBuffer () { return this.buffer }
-  toAddress () { return this.isP2PKH(this.buffer) && new Address(extractP2PKHLockScriptPubkeyhash(this.buffer), require('../index').testnet) }
+  toString () {
+    return this.toHex()
+  }
+
+  toHex () {
+    return encodeHex(this.buffer)
+  }
+
+  toBuffer () {
+    return this.buffer
+  }
 
   get length () { return this.buffer.length }
   slice (start, end) { return this.buffer.slice(start, end) }
-
-  isP2PKH () { return isP2PKHLockScript(this.buffer) }
 
   get chunks () {
     if (SCRIPT_TO_CHUNKS_CACHE.has(this)) return SCRIPT_TO_CHUNKS_CACHE.get(this)
@@ -63,5 +83,22 @@ class Script {
     return chunks
   }
 }
+
+class P2PKHLockScript extends Script {
+  static matches (buffer) {
+    return isP2PKHLockScript(buffer)
+  }
+
+  static fromAddress (address) {
+    return new P2PKHLockScript(createP2PKHLockScript(address.toString()))
+  }
+
+  toAddress () {
+    return Address(extractP2PKHLockScriptPubkeyhash(this.buffer), require('../index').testnet)
+  }
+}
+
+// Further templates may be added at runtime
+Script.templates = { P2PKHLockScript }
 
 module.exports = Script
