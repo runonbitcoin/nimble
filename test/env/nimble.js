3c76a43fd37b691e73bf0f929c4a6c01044edc7f
Brenton Gunning <brentongunning@gmail.com>
2022-04-16 14:34:30 -0600

    Run coverage in unknown variant mode

--- test/env/nimble.js
+++ test/env/nimble.js
@@ -1,13 +1,9 @@
-/* global VARIANT */
-
 // Provides the build of nimble for testing
 
 const path = require('path')
 
 const nimble = process.env.LIB ? require(path.join(process.cwd(), process.env.LIB)) : require('target')
 
-if (typeof VARIANT === 'undefined' && process) {
-  global.VARIANT = 'node'
-}
+global.VARIANT = global.VARIANT || process.env.VARIANT
 
 module.exports = nimble
